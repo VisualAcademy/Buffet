aa0f0199cabcdeab3534a72e07c97fa72e785565
Park YongJune <redplus@redplus.net>
2022-08-22 06:34:27 +0900

    DbSet of T

--- Hawaso/Hawaso/Data/ApplicationDbContext.cs
+++ Hawaso/Hawaso/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Hawaso.Models.Buffets;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hawaso.Data
@@ -9,5 +10,11 @@ namespace Hawaso.Data
             : base(options)
         {
         }
+
+        public DbSet<Broth> Broths { get; set; }
+
+        public DbSet<Noodle> Noodles { get; set; }
+
+        public DbSet<Garnish> Garnishes { get; set; }
     }
 }
