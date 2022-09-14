1c480477dd0c870a22165182bda4d4a79bc19f1a
Park YongJune <redplus@redplus.net>
2022-09-14 22:52:15 +0900

    CandidatesNames to CandidatesIncomes

--- Hawaso/Hawaso/Migrations/20220914134739_IncomesAdd.Designer.cs
+++ Hawaso/Hawaso/Migrations/20220914134739_IncomesAdd.Designer.cs
@@ -4,6 +4,7 @@ using Hawaso.Models.Candidates;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hawaso.Migrations
 {
     [DbContext(typeof(CandidateAppDbContext))]
-    partial class CandidateAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220914134739_IncomesAdd")]
+    partial class IncomesAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
