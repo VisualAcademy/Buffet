89449653ade3615ba9dc74497c71ca43a119e6f3
Park YongJune <redplus@redplus.net>
2023-02-16 09:04:25 +0900

    Convert to file-scoped namespace

--- Hawaso/Hawaso/Pages/Candidates/CandidatesIncomes/ManageTest.cshtml.cs
+++ Hawaso/Hawaso/Pages/Candidates/CandidatesIncomes/ManageTest.cshtml.cs
@@ -3,36 +3,35 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Hawaso.Pages.Candidates.CandidatesIncomes
+namespace Hawaso.Pages.Candidates.CandidatesIncomes;
+
+public class ManageTestModel : PageModel
 {
-    public class ManageTestModel : PageModel
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public string UserId { get; set; }
+
+    public ManageTestModel(UserManager<ApplicationUser> userManager)
     {
-        private readonly UserManager<ApplicationUser> _userManager;
+        _userManager = userManager;
+    }
 
-        public string UserId { get; set; }
+    private async Task LoadAsync(ApplicationUser user)
+    {
+        var userId = await _userManager.GetUserIdAsync(user);
 
-        public ManageTestModel(UserManager<ApplicationUser> userManager)
+        UserId = userId; 
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
         {
-            _userManager = userManager;
+            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
         }
 
-        private async Task LoadAsync(ApplicationUser user)
-        {
-            var userId = await _userManager.GetUserIdAsync(user);
-
-            UserId = userId; 
-        }
-
-        public async Task<IActionResult> OnGetAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await LoadAsync(user);
-            return Page();
-        }
+        await LoadAsync(user);
+        return Page();
     }
 }
