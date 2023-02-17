a190560c5fa959bb6d107979a02f46795b818dd4
Park YongJune <redplus@redplus.net>
2023-02-17 13:14:10 +0900

    Convert to file-scoed namespace

--- Hawaso/Hawaso/Models/Candidates/CandidatesNames/CandidateName.cs
+++ Hawaso/Hawaso/Models/Candidates/CandidatesNames/CandidateName.cs
@@ -1,22 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Hawaso.Models.Candidates.CandidatesNames
+namespace Hawaso.Models.Candidates.CandidatesNames;
+
+public class CandidateName
 {
-    public class CandidateName
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string? FirstName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string? FirstName { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string? LastName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string? LastName { get; set; }
 
-        [StringLength(50)]
-        public string? MiddleName { get; set; }
+    [StringLength(50)]
+    public string? MiddleName { get; set; }
 
-        public string? UserId { get; set; } = null;
-    }
+    public string? UserId { get; set; } = null;
 }
